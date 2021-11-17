3ff34fd212dc2da0c198b722d8fea14fbdfd4057
Ana Labeca <anafrost01@gmail.com>
2021-11-17 15:24:45 +0000

    fix: Remove image from Page component

--- packages/main/src/App.tsx
+++ packages/main/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Navigation } from './components/Navigation/Navigation';
-import Empty from './assets/empty.svg';
 
 import './index.scss';
 
@@ -11,7 +10,6 @@ const App = () => (
     <section className='flex-grow bg-gray-200'>
       <article>
         <h1 className='text-2xl font-bold'>Nothing to see here yet</h1>
-        <img src={Empty} alt='React Logo' />
       </article>
     </section>
   </main>
