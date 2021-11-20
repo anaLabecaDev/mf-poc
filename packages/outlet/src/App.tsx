1a4fb23607f5c575dd6fc5a6a99c9ccea4ffd034
Ana Labeca <anafrost01@gmail.com>
2021-11-20 11:44:44 +0000

    feat(outlet): Create basic components on outlet module

--- packages/outlet/src/App.tsx
+++ packages/outlet/src/App.tsx
@@ -1,14 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DotStatus } from './components/DotStatus/DotStatus';
 
-import "./index.scss";
+import './index.scss';
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: outlet</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+  <div className='w-full h-full flex'>
+    <DotStatus />
+    <em>09904943094</em>
+    <h1>Outlet Name</h1>
+    <address>Outlet address, number</address>
+    <div className='flex flex-row gap-4'>
+      <button className='h-2.5 border-gray-800 text-sm font-bold'>
+        Book Visit
+      </button>
+      <button className='h-2.5 border-gray-800 text-sm font-bold'>
+        Show Route
+      </button>
+    </div>
   </div>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ packages/outlet/src/components/DotStatus/DotStatus.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const DotStatus = () => {
+  return (
+    <div aria-label='Status' className='w-2 h-2 bg-green-500 rounded-full' />
+  );
+};
