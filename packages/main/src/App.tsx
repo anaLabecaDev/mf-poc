b48bea4d039ede8d3025b2c76be7957732fa9243
Ana Labeca <anafrost01@gmail.com>
2021-11-25 14:20:51 +0000

    feat: Login screen

--- packages/main/src/App.tsx
+++ packages/main/src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+// import { Login } from './features/Login/Login';
 import { Navigation } from './features/Navigation/Navigation';
 import './index.scss';
 import store from './state/store';
 
 const App = () => (
   <Provider store={store}>
-    <main className="h-screen flex md:flex-row flex-col-reverse">
-      <Navigation />
-      <section className="flex-grow bg-gray-200">
-        <article className="flex-1 h-full flex justify-center items-center">
-          <h1 className="text-2xl font-bold">Nothing to see here yet</h1>
-        </article>
-      </section>
-    </main>
+    <BrowserRouter>
+      <main className="h-screen flex md:flex-row flex-col-reverse">
+        <Navigation />
+        <section className="flex-grow bg-gray-200">
+          {/* <Login /> */}
+          <article className="flex-1 h-full flex justify-center items-center">
+            <h1 className="text-2xl font-bold">Nothing to see here yet</h1>
+          </article>
+        </section>
+      </main>
+    </BrowserRouter>
   </Provider>
 );
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
--- packages/main/src/features/Login/Login.tsx
+++ packages/main/src/features/Login/Login.tsx
@@ -4,10 +4,32 @@ export const Login = () => {
   const handleLogin = () => console.log('Login');
 
   return (
-    <article className="w-full h-full">
-      <button className="bg-green-400 text-white font-bold py-4 w-80 min-w-max" type="button" onClick={handleLogin}>
-        Log In
-      </button>
+    <article className="w-full h-full bg-gray-200 flex justify-center items-center">
+      <form className="bg-white shadow-md rounded px-8 py-6 max-w-s">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+          />
+          <button className="bg-green-400 text-white font-bold py-2 px-4" type="button" onClick={handleLogin}>
+            Log In
+          </button>
+        </div>
+      </form>
     </article>
   );
 };
